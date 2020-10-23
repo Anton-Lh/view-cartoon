e3e80859da863f8b8c4659258bc26eb91aadbe4e
luoBoB <852951576@qq.com>
2020-10-24 00:19:28 +0800

    完成收藏模块

--- pages/file/file.js
+++ pages/file/file.js
@@ -9,9 +9,12 @@ Page({
   data: {
     mainWidth: 0,  //dom的高度
     mainBoxWidth: 0, //dom的高度
+    marginLF: 0, 
     mainPadding: 0, //dom的padding
     noLoginDisply: 'none', //dom的display
-    dataArr: []
+    moreDisplay: 'block',
+    dataArr: [],
+    pageNum: 1, //页数
   },
 
   /**
@@ -19,23 +22,26 @@ Page({
    */
   onLoad: function (options) {
     var that = this;
-    app.globalData.userInfo = {"user_id":"13667"} //测试代码,上线时删除
+    //app.globalData.userInfo = {"user_id":"13667"} //测试代码,上线时删除
     if(app.globalData.userInfo != null){
       wx.getSystemInfo({
         success: (result) => {
           let sw = result.screenWidth - 30;
           let mbw = sw / 3 - 10;
           let mp = 15;
+          let marginLF = (sw - (mbw*3))/6;
           that.setData({
             mainWidth: sw,
             mainBoxWidth: mbw,
+            marginLF: marginLF,
             mainPadding: mp
           }) 
         },
       })
     }else{
       that.setData({
-        noLoginDisply: 'flex'
+        noLoginDisply: 'flex',
+        moreDisplay: 'none'
       })
     }
   },
@@ -44,7 +50,10 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    this.getCollection();
+    if(app.globalData.userInfo != null){
+      this.getCollection();
+    }
+    
   },
 
   /**
@@ -69,7 +78,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.loadingMore()
   },
   clickLogin: function(){
     wx.redirectTo({
@@ -77,13 +86,36 @@ Page({
     })
   },
   getCollection: function(){
+    var that = this;
     http.post(collection,{
-      "pageNum": 1,
+      "pageNum": that.data.pageNum,
       "pageSize": 10,
       "user_id": app.globalData.userInfo.user_id
       },
       function(res){
-        console.log(res)
+        if(res.message.length != 0){
+          if(that.data.pageNum == 1){
+            that.setData({
+              dataArr: res.message
+            })
+          }else{
+            var dataArr = that.data.dataArr;
+            var arr2 = res.message;
+            dataArr = dataArr.concat(arr2);
+            that.setData({
+              dataArr: dataArr
+            })
+          }
+        }else{
+          that.setData({
+            moreDisplay: 'none'
+          })
+          wx.showToast({
+            title: '没有更多收藏啦~~',
+            icon: 'none',
+            duration: 3000
+          })
+        }
       },
       function(err){
         wx.showToast({
@@ -93,5 +125,17 @@ Page({
         })
       }
     )
+  },
+  loadingMore: function(){
+    var that = this;
+    var pagenum = that.data.pageNum + 1;
+    that.setData({
+      pageNum:pagenum
+    })
+    that.getCollection()
+  },
+  clickMore: function(){
+    
+    this.loadingMore()
   }
 })
